257993d4a5ebffa118032ffd76ccf37768f3076b
CarrotPancake <carrotpancake@noreply>
2019-09-08 21:51:30 +0200

    Add files

---
+++ main.py
@@ -0,0 +1,220 @@
+import __hpx__ as hpx
+
+import re
+import os
+import urllib
+import string
+
+from bs4 import BeautifulSoup
+
+log = hpx.get_logger(__name__)
+
+CHAPTER_PATTERN = re.compile(r"_chapter_\d+\w*$")
+
+H2R_BASE_URL = "https://hentai2read.com"
+
+@hpx.subscribe("init")
+def inited():
+    # Called when this plugin is initialised
+    pass
+
+@hpx.subscribe("disable")
+def disabled():
+    # Called when this plugin has been disiabled
+    pass
+
+@hpx.subscribe("remove")
+def removed():
+    # Called when this plugin is about to be removed
+    pass
+
+@hpx.subscribe('config_update')
+def config_update(cfg):
+    # TODO Anything in the plugin that should check the config?
+    pass
+
+@hpx.attach("Metadata.info")
+def metadata_info():
+    return hpx.command.MetadataInfo(
+        identifier = "h2r-metadata",
+        name = "Hentai2Read Metadata",
+        description = "Extracts and applies metadata from Hentai2Read",
+        sites= ("https://hentai2read.com",),
+        models = (
+            hpx.command.GetDatabaseModel("Gallery"),
+        )
+    )
+
+@hpx.attach("Metadata.query", trigger="h2r-metadata")
+def query(itemtuple):
+    log.info("Trying to fetch metadata from hentai2read")
+    meta_data = []
+
+    for item in itemtuple:
+        sources = item.item.get_sources()
+        if sources:
+            filename = os.path.split(sources[0])[1]
+            filename = os.path.splitext(filename)[0]
+            h2r_name = _to_h2r_name(filename)
+            quoted_filename = urllib.parse.quote(h2r_name)
+
+            h2r_url = urllib.parse.urljoin(H2R_BASE_URL, quoted_filename)
+            # TODO Anything else needed?
+            meta_data.append(hpx.command.MetadataData(
+                    metadataitem = item,
+                    data={
+                        'h2r_url': h2r_url,
+                        }))
+    
+    return tuple(meta_data)
+
+def _to_h2r_name(filename):
+    filename = CHAPTER_PATTERN.sub("", filename)
+    return filename.lower()
+
+@hpx.attach("Metadata.apply", trigger="h2r-metadata")
+def apply(datatuple):
+    log.info("Applying metadata from hentai2read")
+    results = []
+
+    for item in datatuple:
+        h2r_url = item.data["h2r_url"]
+        r = hpx.command.SingleGETRequest().request(h2r_url)
+        # TODO Might want to add logging for not ok response
+        if r.ok:
+            parsed_data = _parse_page(r.text, h2r_url)
+            log.debug(f"Parsed data from response: {parsed_data}")
+            gallery_data = _map_to_hpx_gallery_data(item.item, parsed_data)
+            log.debug(f"About to update item with gallery data: {gallery_data}")
+
+            success = hpx.command.UpdateItemData(item.item, gallery_data)
+            log.info(f"Updated item data: {success}")
+
+            results.append(hpx.command.MetadataResult(data=item, status=success))
+    return tuple(results)
+
+def _parse_page(html, url):
+    meta_data = {
+      "artists": set(),
+      "tags": [],
+      "parodies": [],
+      "url": url,
+      "characters": []
+    }
+
+    soup = BeautifulSoup(html, "html.parser")
+
+    url_pattern = re.compile(re.escape(url))
+    title_elem = soup.find("a", {"href": url_pattern})
+    if title_elem:
+      title = title_elem.get_text()
+      title = title.strip()
+      if title:
+        meta_data["title"] = title
+
+    ignore_text_pattern = re.compile(r"^\d+[\d,. ]*\s?[a-zA-Z]*$")
+    artist_pattern = re.compile(r"/(artist)|(author)/")
+
+    tag_buttons = soup.find_all(class_="tagButton")
+    for tb in tag_buttons:
+        text = tb.get_text()
+        text = text.strip(string.whitespace + string.punctuation)
+        if not text:
+            continue
+        if ignore_text_pattern.search(text):
+            continue
+        if artist_pattern.search(tb["href"]):
+            meta_data["artists"].add(text)
+        elif (_get_list_bullet_name(tb) == "parody"):
+            meta_data["parodies"].append(text)
+        elif (_get_list_bullet_name(tb) == "language"):
+            meta_data["language"] = text
+        elif (_get_list_bullet_name(tb) == "status"):
+            meta_data["status"] = text
+        elif (_get_list_bullet_name(tb) == "storyline"):
+            meta_data["description"] = text
+        elif (_get_list_bullet_name(tb) == "character"):
+            meta_data["characters"].append(text)
+        else:
+            meta_data["tags"].append(text)
+    return meta_data
+
+def _get_list_bullet_name(tag_button):
+    list_bullet = tag_button.find_previous_sibling("b")
+    if not list_bullet:
+        return None
+    
+    bullet_text = list_bullet.get_text()
+    if not bullet_text:
+        return None
+  
+    return bullet_text.lower().strip(string.whitespace + string.punctuation)
+
+def _map_to_hpx_gallery_data(gallery, meta_data):
+    """
+    hpx_model = {
+        'titles': None, # [(title, language),...]
+        'artists': None, # [(artist, (circle, circle, ..)),...]
+        'parodies': None, # [parody, ...]
+        'category': None,
+        'tags': None, # [tag, tag, tag, ..] or {ns:[tag, tag, tag, ...]}
+        'pub_date': None, # DateTime object or Arrow object
+        'language': None,
+        'urls': None # [url, ...]
+    }
+    """
+    gallery_data = hpx.command.GalleryData()
+    
+    if len(meta_data["artists"]) > 0:
+        artists = []
+        for artist in meta_data["artists"]:
+            artist_data = hpx.command.ArtistData(names=[hpx.command.ArtistNameData(name=string.capwords(artist))])
+            artists.append(artist_data)
+        gallery_data.artists = artists
+    
+    title = meta_data.get("title")
+    if title:
+        title_data = hpx.command.TitleData(name=title)
+        title_data.language = hpx.command.LanguageData(name="english")
+        gallery_data.titles = [title_data]
+
+    tags = _map_to_hpx_gallery_tags(meta_data)
+    if len(tags) > 0:
+        gallery_data.tags = tags
+    
+    if len(meta_data["parodies"]) > 0:
+        parodies = []
+        for parody in meta_data["parodies"]:
+            parodies.append(hpx.command.ParodyData(names=[hpx.command.ParodyNameData(name=string.capwords((parody)))]))
+
+        if parodies:
+            gallery_data.parodies = parodies
+    
+    if meta_data.get("url"):
+        gallery_data.urls = [hpx.command.UrlData(name=meta_data["url"])]
+    if meta_data.get("language"):
+        gallery_data.language = hpx.command.LanguageData(name=meta_data["language"])
+    if meta_data.get("status"):
+        # TODO Add if possible
+        pass
+    if meta_data.get("description"):
+        # TODO Add if possible
+        pass
+    
+    return gallery_data
+
+def _map_to_hpx_gallery_tags(meta_data):
+    tags = []
+
+    if len(meta_data["tags"]) > 0:
+        for tag in meta_data["tags"]:
+            tag_data = hpx.command.TagData(name=tag)
+            tags.append(hpx.command.NamespaceTagData(tag=tag_data))
+
+    if len(meta_data["characters"]) > 0:
+        for tag in meta_data["characters"]:
+            tag_data = hpx.command.TagData(name=tag)
+            namespace_data = hpx.command.NamespaceData(name="character")
+            tags.append(hpx.command.NamespaceTagData(tag=tag_data, namespace=namespace_data))
+    
+    return tags
